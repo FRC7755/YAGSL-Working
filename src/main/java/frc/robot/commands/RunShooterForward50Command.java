20cf17de87179da825cb5b0b2d569a0b26ddc47e
FRC7755 <146410914+FRC7755@users.noreply.github.com>
2024-10-08 08:42:06 -0700

    Added Shooter speeds

---
+++ src/main/java/frc/robot/commands/RunShooterForward50Command.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class RunShooterForward50Command extends Command {
+  /** Creates a new ClimberDownCommand. */
+
+  private final ShooterSubsystem shooterSubsystem;
+
+  public RunShooterForward50Command(ShooterSubsystem subsystem) {
+    shooterSubsystem = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements( shooterSubsystem );
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    shooterSubsystem.ShooterOn50Forward();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    shooterSubsystem.ShooterStop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}

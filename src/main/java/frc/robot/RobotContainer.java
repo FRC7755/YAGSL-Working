611d847af39c6acc67f1d5027d696db28ef85e99
FRC7755 <146410914+FRC7755@users.noreply.github.com>
2024-10-17 21:03:05 -0700

    Red Auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -292,7 +292,7 @@ public class RobotContainer
   public Command getAutonomousCommand()
   {
     // An example command will be run in autonomous
-   return new PathPlannerAuto("Blue Right Speaker to Right Ring");
+   return new PathPlannerAuto("Blue Speaker to Center Line");
 //    return autoChooser.getSelected();
    // return null;
   }
